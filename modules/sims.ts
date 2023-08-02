088cf0c51d2d61e1147bde5a587e996a4d5b99c5
Shie1 <sonkolyw@gmail.com>
2023-08-02 05:59:05 +0200

    find DLC source

--- modules/sims.ts
+++ modules/sims.ts
@@ -1,6 +1,8 @@
 import { spawn } from "child_process"
 import { accessSync, readFileSync } from "fs"
 import { parse } from "ini"
+import { globSync } from "fast-glob"
+import { join } from "path"
 
 const DLCs = parse(readFileSync("./dlc.ini", "utf-8"))
 
@@ -86,8 +88,14 @@ export class Sims4DLC {
         }
     }
 
-    openURL() {
-        spawn("explorer", [`https://www.ea.com/games/the-sims/the-sims-4/store/addons/${this.name.toLowerCase().replace(/ /g, "-")}`])
+    openFolder() {
+        if (!this.installed) {
+            return false
+        }
+        spawn('explorer', [`${this.instance.path}\\${this.id}`], {
+            detached: true,
+        })
+        return true
     }
 }
 
@@ -100,7 +108,7 @@ export class Sims4Instance {
     }
 
     getDLCs() {
-        return Object.keys(DLCs).map((item: string) => new Sims4DLC(this, DLCs[item].name, item))
+        return Object.keys(DLCs).map((item: string) => new Sims4DLC(this, DLCs[item]["Name_en_US"], item))
     }
 
     launch() {
@@ -108,4 +116,91 @@ export class Sims4Instance {
             detached: true,
         })
     }
+}
+
+export const isDLCDrive = (drive: string) => {
+    try {
+        accessSync(`${drive}\\EP01\\Worlds\\Areas\\EP01_AlienWorld_01.world`)
+    } catch (e) {
+        return false
+    }
+    return true
+}
+
+export const getDLCDrive = async () => {
+    const drives = await getPartitions()
+    const dlcDrive = drives.filter(isDLCDrive)
+    if (dlcDrive.length === 0) {
+        return undefined
+    }
+    return new DLCDrive(dlcDrive[0])
+}
+
+export const getDownloadsFolder = () => {
+    // (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+    return new Promise<string>((resolve) => {
+        const process = spawn('powershell')
+        process.stdin.write('(New-Object -ComObject Shell.Application).NameSpace(\'shell:Downloads\').Self.Path\n');
+        process.stdout.on('data', (data) => {
+            if (data.toString().trim().match(/^[A-Z]:\\/)) {
+                resolve(data.toString().trim())
+                process.stdin.end();
+            }
+        })
+    })
+}
+
+export const getDLCISO = async () => {
+    const dlFolder = await getDownloadsFolder()
+    const filePath = (await globSync("*.iso", {
+        onlyFiles: true,
+        cwd: dlFolder,
+    })).filter((item: string) => item.toLowerCase().search("DLC") !== -1)[0]
+    return join(dlFolder, filePath)
+}
+
+export const mountISO = (path: string) => {
+    // eslint-disable-next-line no-async-promise-executor
+    return new Promise<string>(async (resolve) => {
+        const partitionsBeforeMount = await getPartitions()
+        const process = spawn('powershell')
+        process.stdin.write(`Mount-DiskImage -ImagePath "${path}"\n`);
+        let psCount = 0
+        process.stdout.on('data', (data) => {
+            if (data.toString().trim().startsWith("PS")) {
+                psCount++
+                if (psCount === 2) {
+                    getPartitions().then((partitions: string[]) => {
+                        const newPartitions = partitions.filter((item: string) => partitionsBeforeMount.indexOf(item) === -1)
+                        if (newPartitions.length > 0) {
+                            resolve(newPartitions[0])
+                        } else {
+                            resolve(undefined)
+                        }
+                    })
+                }
+            }
+        })
+        process.stdin.end();
+    })
+}
+
+export const unmountISO = (path: string) => {
+    if (path.length !== 1) {
+        throw new Error("The path provided is not a drive letter!")
+    }
+    const process = spawn('powershell')
+    process.stdin.write(`$driveEject = New-Object -comObject Shell.Application\n$driveEject.Namespace(17).ParseName("${path}:").InvokeVerb("Eject") `);
+    process.stdin.end();
+}
+
+export class DLCDrive {
+    public readonly contents: Array<string> = []
+    constructor(
+        public readonly path: string
+    ) {
+        this.contents = globSync("*").filter((item: string) => Object.keys(DLCs).indexOf(item) !== -1)
+    }
+
+
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { Sims4Instance, isSims4Folder, locateSims4 } from '../modules/sims'
-import { ConfigProvider, Layout, theme, message } from 'antd'
+import { Sims4Instance, isDLCDrive, isSims4Folder, locateSims4, getDLCISO, mountISO, getDLCDrive, unmountISO } from '../modules/sims'
+import { ConfigProvider, Layout, theme, message, Button, Alert, Modal } from 'antd'
 import './globals.css'
-import { useEffect, useRef, useState } from 'react'
-import { motion } from 'framer-motion'
-import { contextBridge, ipcRenderer } from "electron"
+import { useEffect, useRef, useState, createContext, useContext } from 'react'
+import { IconDeviceFloppy } from "@tabler/icons-react"
+import { AnimatePresence, motion } from 'framer-motion'
+import { ipcRenderer } from "electron"
+import { DLCDrive, Sims4DLC } from '../modules/sims'
+
+const GlobalSettingsContext = createContext<{ globalSettings: GlobalSettings, setGlobalSettings: (value: GlobalSettings) => void }>({ globalSettings: { dlcDrive: undefined }, setGlobalSettings: () => { "" } })
 
 const InstanceTab = ({ instance, active, onClick, onRemove }: {
     instance: Sims4Instance,
@@ -14,8 +18,6 @@ const InstanceTab = ({ instance, active, onClick, onRemove }: {
     const [hover, setHover] = useState<boolean>(false)
     const ref = useRef<HTMLDivElement>(null)
     return (<motion.div
-        animate={{
-        }}
         style={{
             display: 'inline-block',
             padding: '0 10px',
@@ -49,9 +51,165 @@ const InstanceTab = ({ instance, active, onClick, onRemove }: {
     </motion.div>)
 }
 
+const DLC = ({ dlc }: { dlc: Sims4DLC }) => {
+    return (<div style={{
+        aspectRatio: '16 / 9',
+        userSelect: 'none',
+        minHeight: 120,
+        padding: 10,
+        display: 'flex',
+        flexDirection: 'column',
+        flex: 1,
+    }} onClick={() => {
+        if (!dlc.openFolder()) {
+            message.error('DLC is not installed!')
+        }
+    }}>
+        <div style={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <h2 style={{
+                fontSize: 40,
+                marginBottom: 10,
+            }}>
+                {dlc.id}
+            </h2>
+        </div>
+        <div>
+            <span style={{
+            }}>
+                {dlc.name}
+            </span>
+            <span style={{
+            }}>
+                {dlc.installed ? '🟢' : '🔴'}
+            </span>
+        </div>
+    </div>)
+}
+
+const InstanceDashboard = ({ instance }: { instance: Sims4Instance }) => {
+    if (!instance) return <></>
+    const { globalSettings, setGlobalSettings } = useContext(GlobalSettingsContext)
+    const DLCs = instance.getDLCs()
+    return (<>
+        <AnimatePresence>
+            {globalSettings.dlcDrive === undefined &&
+                <motion.div
+                    style={{ overflow: 'hidden', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                    initial={{ height: 0 }}
+                    animate={{ height: 'auto' }}
+                    exit={{ height: 0 }}
+                >
+                    <Alert
+                        style={{
+                            margin: '10px 0',
+                        }}
+                        type='warning'
+                        message="No DLC source drive found, please select a drive or directory manually!"
+                        action={<Button style={{
+                            marginLeft: 10,
+                            height: 32,
+                        }} size="small" type="text" onClick={() => {
+                            ipcRenderer.invoke('folder-select', {
+                                title: 'Select DLC Drive/Directory',
+                            }).then((res) => {
+                                if (res.canceled) return
+                                if (res.filePaths.length === 0) return
+                                if (!isDLCDrive(res.filePaths[0])) {
+                                    message.error('Selected folder is not a DLC drive!')
+                                    return
+                                }
+                                setGlobalSettings({ ...globalSettings, dlcDrive: res.filePaths[0] })
+                            })
+                        }}>
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    padding: '4px 0',
+                                }}
+                            >
+                                <span style={{
+                                    lineHeight: 24,
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    height: 24,
+                                    marginRight: 5,
+                                }}><IconDeviceFloppy size={20} /></span>
+                                <span style={{
+                                    lineHeight: 24,
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    height: 24,
+                                }}>Select DLC Source</span>
+                            </div>
+                        </Button>} />
+                </motion.div>
+            }
+        </AnimatePresence >
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+                alignItems: 'center',
+                overflowY: 'auto',
+                flex: 1,
+                flexShrink: 1,
+            }}>
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                    alignItems: 'center',
+                }}
+            >
+                {DLCs.map((dlc, key) => {
+                    return (<DLC key={key} dlc={dlc} />)
+                })}
+            </div>
+        </div >
+    </>)
+}
+
+export type GlobalSettings = {
+    dlcDrive: DLCDrive | undefined,
+}
+
 const App = () => {
     const [simsInstances, setSimsInstances] = useState<Sims4Instance[]>([])
     const [activeInstance, setActiveInstance] = useState<number>(0)
+    const [globalSettings, setGlobalSettings] = useState<GlobalSettings>({
+        dlcDrive: undefined,
+    })
+    const [dlcISO, setDLCISO] = useState<string | undefined>(undefined)
+    const [dlcISOModal, setDLCISOModal] = useState<boolean>(false)
+
+    useEffect(() => {
+        getDLCDrive().then((res) => {
+            if (res) {
+                setGlobalSettings({ ...globalSettings, dlcDrive: res })
+            } else {
+                if (globalSettings.dlcDrive) {
+                    setDLCISOModal(false)
+                    return
+                }
+                if (dlcISO) {
+                    return
+                }
+                getDLCISO().then((res) => {
+                    if (res) {
+                        setDLCISOModal(true)
+                        setDLCISO(res)
+                    }
+                    else {
+                        setDLCISOModal(false)
+                    }
+                })
+            }
+        })
+    }, [globalSettings.dlcDrive, dlcISO])
 
     useEffect(() => {
         locateSims4().then((res) => {
@@ -73,71 +231,98 @@ const App = () => {
     }, [activeInstance, simsInstances])
 
     return (<>
-        <ConfigProvider theme={{
-            algorithm: theme.darkAlgorithm,
-            token: {
-                colorPrimary: '#8B0000', // Dark red color
-            },
-        }}>
-            <Layout>
-                <Layout.Content style={{ minHeight: '100dvh', background: "#1f1f1f" }}>
-                    <motion.header layout layoutDependency={simsInstances}
-                        style={{
-                            backgroundColor: '#171717',
-                            borderBottom: '4px solid black',
-                            width: '100vw',
-                            flexWrap: 'wrap',
-                            display: 'flex',
-                            overflow: 'hidden',
-                            flexDirection: 'row',
-                        }}>
-                        {/* map sims instances as tabs */}
-                        {simsInstances.map((instance, key) => {
-                            const active = key === activeInstance
-                            if (key + 1 === simsInstances.length) return (<span style={{
-                                display: 'flex',
-                                flexDirection: 'row',
-                                alignItems: 'center',
-                                height: 40,
-                                flexGrow: 1,
+        <GlobalSettingsContext.Provider value={{ globalSettings, setGlobalSettings }}>
+            <ConfigProvider theme={{
+                algorithm: theme.darkAlgorithm,
+                token: {
+                    colorPrimary: '#8B0000', // Dark red color
+                },
+            }}>
+                <Layout>
+                    <Layout.Content style={{
+                        height: '100dvh',
+                        background: "#1f1f1f",
+                        display: 'flex',
+                        flexDirection: 'column',
+                    }}>
+                        <Modal
+                            open={dlcISOModal}
+                            onCancel={() => setDLCISOModal(false)}
+                            title="DLC ISO found!"
+                            okText="Mount"
+                            cancelText="Ignore"
+                            onOk={() => {
+                                mountISO(dlcISO).then((disk) => {
+                                    if (!isDLCDrive(disk)) {
+                                        message.error('The mounted ISO is not a DLC drive!')
+                                        unmountISO(disk)
+                                        setDLCISOModal(false)
+                                        return
+                                    }
+                                    setGlobalSettings({ ...globalSettings, dlcDrive: new DLCDrive(disk) })
+                                })
                             }}>
-                                <InstanceTab key={key} onRemove={() => {
-                                    setSimsInstances(simsInstances.filter((_, i) => i !== key))
-                                }
-                                } instance={instance} active={active} onClick={() => setActiveInstance(key)} />
-                                <div key="add-instance" style={{
-                                    color: '#ffffff',
-                                    lineHeight: 20,
-                                    padding: '0 10px',
-                                    cursor: 'pointer',
-                                    fontSize: 25,
-                                    marginLeft: "auto",
-                                    flexBasis: 50,
-                                    userSelect: 'none',
-                                }} onClick={() => {
-                                    ipcRenderer.invoke('folder-select', {
-                                        title: 'Select Sims 4 Folder',
-                                    }).then((res) => {
-                                        if (res.canceled) return
-                                        if (res.filePaths.length === 0) return
-                                        if (isSims4Folder(res.filePaths[0]) === false) {
-                                            message.error('Selected folder is not a Sims 4 folder!')
-                                            return
-                                        }
-                                        setSimsInstances([...simsInstances, res.filePaths[0]])
-                                    })
+                            <p style={{ marginBottom: 0 }}>Found a DLC ISO at <i>"{dlcISO}"</i></p>
+                        </Modal>
+                        <motion.header layout layoutDependency={simsInstances}
+                            style={{
+                                backgroundColor: '#171717',
+                                borderBottom: '4px solid black',
+                                width: '100vw',
+                                flexWrap: 'wrap',
+                                display: 'flex',
+                                overflow: 'hidden',
+                                flexDirection: 'row',
+                            }}>
+                            {/* map sims instances as tabs */}
+                            {simsInstances.map((instance, key) => {
+                                const active = key === activeInstance
+                                if (key + 1 === simsInstances.length) return (<span key={key} style={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    height: 40,
+                                    flexGrow: 1,
                                 }}>
-                                    +
-                                </div>
-                            </span>)
-                            return <InstanceTab key={key} onRemove={() => {
-                                setSimsInstances(simsInstances.filter((_, i) => i !== key))
-                            }} instance={instance} active={active} onClick={() => setActiveInstance(key)} />
-                        })}
-                    </motion.header>
-                </Layout.Content>
-            </Layout>
-        </ConfigProvider>
+                                    <InstanceTab key={key} onRemove={() => {
+                                        setSimsInstances(simsInstances.filter((_, i) => i !== key))
+                                    }
+                                    } instance={instance} active={active} onClick={() => setActiveInstance(key)} />
+                                    <div key="add-instance" style={{
+                                        color: '#ffffff',
+                                        lineHeight: 20,
+                                        padding: '0 10px',
+                                        cursor: 'pointer',
+                                        fontSize: 25,
+                                        marginLeft: "auto",
+                                        flexBasis: 50,
+                                        userSelect: 'none',
+                                    }} onClick={() => {
+                                        ipcRenderer.invoke('folder-select', {
+                                            title: 'Select Sims 4 Folder',
+                                        }).then((res) => {
+                                            if (res.canceled) return
+                                            if (res.filePaths.length === 0) return
+                                            if (isSims4Folder(res.filePaths[0]) === false) {
+                                                message.error('Selected folder is not a Sims 4 folder!')
+                                                return
+                                            }
+                                            setSimsInstances([...simsInstances, res.filePaths[0]])
+                                        })
+                                    }}>
+                                        +
+                                    </div>
+                                </span>)
+                                return <InstanceTab key={key} onRemove={() => {
+                                    setSimsInstances(simsInstances.filter((_, i) => i !== key))
+                                }} instance={instance} active={active} onClick={() => setActiveInstance(key)} />
+                            })}
+                        </motion.header>
+                        <InstanceDashboard instance={simsInstances[activeInstance]} />
+                    </Layout.Content>
+                </Layout>
+            </ConfigProvider>
+        </GlobalSettingsContext.Provider >
     </>)
 }
 
